112d3f4bf406fd237d751caa657b4f5c5d7a726f
Jack Kolb <jack@jackkolb.com>
2022-11-10 14:39:01 -0500

    updated cd

--- node/main.py
+++ node/main.py
@@ -126,7 +126,7 @@ def startProject(project_name):
     git_clone.wait()
     # open the process
     subprocess.Popen(["chmod", "+x", "./projects/" + project_name + "/run.sh"])
-    projects[project_name]["process"] = subprocess.Popen(["cd", "./projects/" + project_name, ";", "sh", "run.sh"], env=projects[project_name]["environment-variables"])
+    projects[project_name]["process"] = subprocess.Popen(["sh", "run.sh"], cwd="./projects/" + project_name, env=projects[project_name]["environment-variables"])
     print("[info] project started: " + project_name)
 
 # pulls from git to check for updates
